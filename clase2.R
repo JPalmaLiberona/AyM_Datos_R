0435a85aa4f1beef201cfb1ece2faff3dbbf949f
José Antonio Palma Liberona <JpalmaLiberona@gmail.com>
2020-08-18 11:17:51 -0400

    version final clase 2

--- clase2.R
+++ clase2.R
@@ -1,4 +1,20 @@
 ### Clase 2 curso analisis y manipulacion de datos en R
+# cargar paquetes
+
 library(tidyverse)
 library(knitr)
 library(kableExtra)
+
+#importar datos
+
+plants <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv")
+actions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv")
+threats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv")
+
+
+data("msleep")
+Tabla <- msleep %>% group_by(vore) %>% 
+  summarise_at("sleep_total", .funs = list(Mean = mean, SD = sd)) %>% 
+  dplyr::filter((!is.na(vore))) %>% 
+  arrange(desc(Mean))
+Tabla
